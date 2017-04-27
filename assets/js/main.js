f1911ce91cb8a3d46f4e51e5a1e92cb033b4f066
kumelkalen <free.your.mind.fc@gmail.com>
2017-04-27 14:00:31 -0300

    trabajo terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,71 @@
 var res = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
-  }
-  ];
+{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 2,
+  "title": "quis ut nam facilis et officia qui",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 3,
+  "title": "fugiat veniam minus",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 4,
+  "title": "et porro tempora",
+  "completed": true
+},
+{
+  "userId": 1,
+  "id": 5,
+  "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 6,
+  "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 7,
+  "title": "illo expedita consequatur quia in",
+  "completed": false
+},
+{
+  "userId": 1,
+  "id": 8,
+  "title": "quo adipisci enim quam ut ab",
+  "completed": true
+},
+{
+  "userId": 1,
+  "id": 9,
+  "title": "molestiae perspiciatis ipsa",
+  "completed": false
+}
+];
 
-  var imprimir=res.forEach(function(el){
-var lis=document.createElement("LI");
-var texto=document.createTextNode(el.title);
-lis.appendChild(texto);
-document.getElementById("lista").appendChild(lis)
+var imprimir=res.forEach(function(el){
+  var lis=document.createElement("LI");
+  var texto=document.createTextNode(el.title);
+  lis.appendChild(texto);
+  document.getElementById("lista").appendChild(lis)
 })
 
-  function agregarTarea(){
+function agregarTarea(){
   var tarea =document.getElementById("lista");
   var ingresar = document.getElementById("agregarli").value
   tarea.innerHTML += '<li>' + ingresar + '</li>';
 }
-  
+
 agregarTarea();
